9fe114a57466582b34476d9239d52c0faed523f1
gygesM <l2241883@gmail.com>
2016-06-20 12:13:42 +0800

    lla

---
+++ src/main/java/com/gyges/mvnstudy/App.java
@@ -0,0 +1,13 @@
+package com.gyges.mvnstudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
